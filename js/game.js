1483443ff0d7d6ba4fddcaa7e4b8502a782db74b
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-04-19 16:35:15 +0000

    Made screen scale with device screen size

--- js/game.js
+++ js/game.js
@@ -2,13 +2,26 @@
 
 // Cristiano
 // ICS2O-Space-Aliens
-// April 13 2022
+// April 13-19 2022
 
+// Scene
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  // Set BG colour
+  backgroundColor: 0x5f6e7a,
+  // Set screen in the middle of page
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)
